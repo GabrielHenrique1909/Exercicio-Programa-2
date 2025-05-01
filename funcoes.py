bb10a2817ce4d029eb2c15d056837ca68d8f70b0
Gabriel Henrique Silva <gabrielhs2@al.insper.edu.br>
2025-05-01 17:16:33 -0300

    Guardar dado

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,10 @@ def rolar_dados(n):
     for i in range(n):
         lista.append(randint(1,6))
     return lista
+
+def guardar_dado(dadosrolados, dadosguardados, dadoparaguardar):
+    for i in range(len(dadosrolados)):
+        if i == dadoparaguardar:
+            dadosguardados.append(dadosrolados[i])
+            del dadosrolados[i]
+    return [dadosrolados,dadosguardados]        
