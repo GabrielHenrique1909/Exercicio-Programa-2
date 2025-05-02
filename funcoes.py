9a2000cc4dd778718567e68ec6c17e0ec0346fbf
anthoni-serro <anthonimcs@al.insper.edu.br>
2025-05-02 14:33:21 -0300

    Calcula pontos na regra avançada - Sequência alta

--- funcoes.py
+++ funcoes.py
@@ -32,5 +32,22 @@ def calcula_pontos_soma(dados):
         soma += dado
     return soma
 
-gvjkgv igi 
- 
+def calcula_pontos_sequencia_baixa(dados):   
+    dic = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for dado in dados:
+        dic[dado]+= 1
+    if dic[1]>0 and dic[2]>0 and dic[3]>0 and dic[4]>0 or dic[2]>0 and dic[3]>0 and dic[4]>0 and dic[5]>0 or dic[3]>0 and dic[4]>0 and dic[5]>0 and dic[6]>0:
+        return 15
+    else:
+        return 0
+
+def calcula_pontos_sequencia_alta(dados):
+    dic = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    for dado in dados:
+        dic[dado]+= 1
+    if dic[1]>0 and dic[2]>0 and dic[3]>0 and dic[4]>0 and dic[5]>0 or dic[2]>0 and dic[3]>0 and dic[4]>0 and dic[5]>0 and dic[6]>0:
+        return 30
+    else: 
+        return 0
+    
+
